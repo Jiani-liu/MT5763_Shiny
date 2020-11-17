e799a38175394442a62eddaa3dd5764319e3cf83
Jiani Liu <jianiliu2015@hotmail.com>
2020-11-17 17:18:49 +0800

    library relevant packages

---
+++ app.R
@@ -0,0 +1,25 @@
+library(shinydashboard)
+library(dashboardthemes)
+library(shinyjs)
+library(shinycssloaders)
+library(shinyWidgets)
+library(jsonlite)
+library(httr)
+library(htmltools)
+library(htmlwidgets)
+library(geojsonio)
+library(furrr)
+
+# data wrangling
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(magrittr)
+
+# visualization
+library(ggplot2)
+library(plotly)
+library(scales)
+library(glue)
+library(leaflet)
+library(DT)
